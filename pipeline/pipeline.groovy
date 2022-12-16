d14d6bee7550666daea52ceb6e51fe0317220875
alexandrebdealencar <62028941+alexandrebdealencar@users.noreply.github.com>
2022-12-16 20:52:10 -0300

    Update pipeline.groovy

--- pipeline/pipeline.groovy
+++ pipeline/pipeline.groovy
@@ -8,11 +8,11 @@ node('docker-node') {
     deleteDir()
     checkout scm
   }
-  stage('SonarQube Analysis') {
-    withSonarQubeEnv('sonar-poc') {
-      sh "/usr/share/maven/bin/mvn clean verify sonar:sonar -Dsonar.projectKey=finance-status"
-    }
-  }
+//  stage('SonarQube Analysis') {
+//    withSonarQubeEnv('sonar-poc') {
+//      sh "/usr/share/maven/bin/mvn clean verify sonar:sonar -Dsonar.projectKey=finance-status"
+//    }
+//  }
   stage('Build Image'){
       image = docker.build("$name_img")
     }
