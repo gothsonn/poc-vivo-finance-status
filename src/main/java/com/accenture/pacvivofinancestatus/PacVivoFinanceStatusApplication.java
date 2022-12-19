2288ad603d609fdf7431324ca1455f8dd3809771
Augusto Daniel <augusto.danieldesouza.ext@bayer.com>
2022-12-19 10:01:07 -0300

    Criacao do Swagger

--- src/main/java/com/accenture/pacvivofinancestatus/PacVivoFinanceStatusApplication.java
+++ src/main/java/com/accenture/pacvivofinancestatus/PacVivoFinanceStatusApplication.java
@@ -1,13 +1,34 @@
 package com.accenture.pacvivofinancestatus;
 
+import com.accenture.pacvivofinancestatus.configurations.LocalDateConverter;
+import com.accenture.pacvivofinancestatus.configurations.LocalDateTimeConverter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
+@ComponentScan(basePackages = {
+		"com.accenture.pacvivofinancestatus",
+		"com.accenture.pacvivofinancestatus.api" ,
+		"com.accenture.pacvivofinancestatus.configurations"
+})
 public class PacVivoFinanceStatusApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PacVivoFinanceStatusApplication.class, args);
 	}
 
+	@Configuration
+	static class MyConfig extends WebMvcConfigurerAdapter {
+		@Override
+		public void addFormatters(FormatterRegistry registry) {
+			registry.addConverter(new LocalDateConverter("yyyy-MM-dd"));
+			registry.addConverter(new LocalDateTimeConverter("yyyy-MM-dd'T'HH:mm:ss.SSS"));
+		}
+	}
 }
---
+++ src/main/java/com/accenture/pacvivofinancestatus/configurations/SwaggerDocumentationConfig.java
@@ -0,0 +1,42 @@
+package com.accenture.pacvivofinancestatus.configurations;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2022-11-06T21:16:01.493Z")
+
+@Configuration
+@EnableSwagger2
+public class SwaggerDocumentationConfig {
+
+    ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Poc Vivo Finance Status")
+                .description("CustomerBill Management")
+                .license("")
+                .licenseUrl("http://unlicense.org")
+                .termsOfServiceUrl("")
+                .version("4.0.0")
+                .contact(new Contact("ACCENTURE", "", ""))
+                .build();
+    }
+
+    @Bean
+    public Docket customImplementation() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.accenture.pacvivofinancestatus"))
+                .build()
+                .directModelSubstitute(java.time.OffsetDateTime.class, java.sql.Date.class)
+                .directModelSubstitute(java.time.OffsetDateTime.class, java.util.Date.class)
+                .apiInfo(apiInfo());
+    }
+
+}
