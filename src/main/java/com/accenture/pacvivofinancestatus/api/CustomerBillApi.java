5c54ae39fe3c70dea48fa3ee3efdccb97238f90f
Augusto Daniel <augusto.danieldesouza.ext@bayer.com>
2022-12-19 10:46:33 -0300

    Criacao do Swagger

--- src/main/java/com/accenture/pacvivofinancestatus/api/CustomerBillApi.java
+++ src/main/java/com/accenture/pacvivofinancestatus/api/CustomerBillApi.java
@@ -18,8 +18,8 @@ import javax.validation.Valid;
 @javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2022-12-17T13:51:37.299Z")
 
 @Validated
-@Api(value = "customerBill", description = "the customerBill API")
-@RequestMapping(value = "/customerBillManagement/")
+@Api(value = "customerBillManagement", description = "the customerBill API")
+@RequestMapping(value = "/customerBillManagement")
 public interface CustomerBillApi {
 
     @ApiOperation(value = "Retrieves a CustomerBill by ID", nickname = "retrieveCustomerBill", notes = "This operation retrieves a CustomerBill entity. Attribute selection is enabled for all first level attributes.", response = CustomerBill.class, tags={ "customerBill", })
@@ -37,5 +37,22 @@ public interface CustomerBillApi {
         consumes = { "application/json;charset=utf-8" },
         method = RequestMethod.GET)
     ResponseEntity<CustomerBill> retrieveCustomerBill(@ApiParam(value = "Identifier of the CustomerBill",required=true) @PathVariable("id") String id,@ApiParam(value = "Comma-separated properties to provide in response") @Valid @RequestParam(value = "fields", required = false) String fields);
+
+
+    @ApiOperation(value = "Retrieves a CustomerBill by ID", nickname = "retrieveCustomerBill", notes = "This operation retrieves a CustomerBill entity. Attribute selection is enabled for all first level attributes.", response = CustomerBill.class, tags={ "customerBill", })
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Success", response = CustomerBill.class),
+            @ApiResponse(code = 400, message = "Bad Request", response = Error.class),
+            @ApiResponse(code = 401, message = "Unauthorized", response = Error.class),
+            @ApiResponse(code = 403, message = "Forbidden", response = Error.class),
+            @ApiResponse(code = 404, message = "Not Found", response = Error.class),
+            @ApiResponse(code = 405, message = "Method Not allowed", response = Error.class),
+            @ApiResponse(code = 409, message = "Conflict", response = Error.class),
+            @ApiResponse(code = 500, message = "Internal Server Error", response = Error.class) })
+    @RequestMapping(value = "/customer/{id}",
+            produces = { "application/json;charset=utf-8" },
+            consumes = { "application/json;charset=utf-8" },
+            method = RequestMethod.GET)
+    ResponseEntity<CustomerBill> requestMock(@ApiParam(value = "Identifier of the CustomerBill",required=true) @PathVariable("id") String id,@ApiParam(value = "Comma-separated properties to provide in response") @Valid @RequestParam(value = "fields", required = false) String fields);
 
 }
